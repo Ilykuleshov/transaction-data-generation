40d3a55004df74951552cf5a8a1f43e5413b9177
nokiroki <nikita@nokiroki.ru>
2023-04-28 16:29:29 +0300

    Ready LSTM (not tested)

--- src/networks/lstm/lstm.py
+++ src/networks/lstm/lstm.py
@@ -6,6 +6,7 @@ from pytorch_lightning import LightningModule
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 from src.networks.common_layers import PositionalEncoding
@@ -20,8 +21,11 @@ class LSTMAE(LightningModule):
         num_layers: int,
         mcc_embed_dim: int,
         n_vocab_size: int,
+        loss_weights: tuple[float, float, float],
         freeze_embed: bool,
         unfreeze_after: int,
+        lr: float,
+        weight_decay: float,
         *args: typing.Any, **kwargs: typing.Any
     ) -> None:
         super().__init__(*args, **kwargs)
@@ -32,8 +36,11 @@ class LSTMAE(LightningModule):
             'num_layers'    : num_layers,
             'mcc_embed_dim' : mcc_embed_dim,
             'n_vocab_size'  : n_vocab_size,
+            'loss_weights'  : loss_weights,
             'freeze_embed'  : freeze_embed,
-            'unfreeze_after': unfreeze_after
+            'unfreeze_after': unfreeze_after,
+            'lr'            : lr,
+            'weight_decay'  : weight_decay
         })
 
         self.mcc_embed = nn.Embedding(n_vocab_size + 1, mcc_embed_dim, padding_idx=0)
@@ -73,6 +80,14 @@ class LSTMAE(LightningModule):
         self.out_binary = nn.Linear(2 * embed_dim, 1)
         self.out_mcc    = nn.Linear(2 * embed_dim, n_vocab_size)
 
+        self.amount_loss_weights    = loss_weights[0]
+        self.binary_loss_weights    = loss_weights[1]
+        self.mcc_loss_weights       = loss_weights[2]
+
+        self.mcc_criterion = nn.CrossEntropyLoss(ignore_index=0)
+        self.binary_criterion = nn.BCELoss()
+        self.amount_criterion = nn.MSELoss()
+
         self.train_time: float = None
 
     # Set pretrained tr2vec weights
@@ -101,7 +116,7 @@ class LSTMAE(LightningModule):
         is_income: torch.Tensor,
         transaction_amt: torch.Tensor,
         lengths: torch.Tensor
-    ) -> None:
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         mcc_embed = self.mcc_embed(mcc_codes)
         mcc_embed = self.pe(mcc_embed)
 
@@ -130,19 +145,100 @@ class LSTMAE(LightningModule):
         is_income_rec = self.out_binary(seqs_after_lstm)
         amount_rec = self.out_amount(seqs_after_lstm)
 
+        # for is_income we must maintain sigmoid layer by ourselfs
+        # as BCEWithLogits will provide for all paddings .5 probability
+        is_income_rec = F.sigmoid(is_income_rec)
+
         mcc_rec, is_income_rec, amount_rec = self._trim_out_seq(
             [mcc_rec, is_income_rec, amount_rec]
         )
 
-        return mcc_rec, is_income_rec, amount_rec
+        # squeeze for income and amount is required to reduce last 1 dimension
+        return mcc_rec, is_income_rec.squeeze(), amount_rec.squeeze()
 
     # Method for returning original padding
     @staticmethod
     def _trim_out_seq(
-        seqs_to_trim: typing.List[torch.Tensor],
+        seqs_to_trim: tuple[torch.Tensor, ...],
         lengths: torch.Tensor
-    ) -> typing.List[torch.Tensor]:
+    ) -> tuple[torch.Tensor, ...]:
         max_length = seqs_to_trim[0].shape[1]
         mask = torch.arange(max_length) \
                 .expand(len(lengths), max_length) < lengths.unsqueeze(1)
         return list(map(lambda seq: seq.masked_fill_(~mask), seqs_to_trim))
+    
+    def _calculate_losses(self, batch: tuple[
+        torch.LongTensor,
+        torch.LongTensor,
+        torch.LongTensor,
+        torch.DoubleTensor,
+        int,
+        float
+    ]) -> float:
+        mcc_rec, is_income_rec, amount_rec = self(*batch[:-1])
+        mcc_rec = torch.permute(mcc_rec, (0, 2, 1))
+        
+        mcc_loss = self.mcc_criterion(mcc_rec, batch[1])
+        binary_loss = self.binary_criterion(is_income_rec, batch[2])
+        amount_loss = self.amount_criterion(amount_rec, batch[3])
+
+        return self.mcc_loss_weights * mcc_loss + \
+            self.binary_loss_weights * binary_loss + \
+            self.amount_loss_weights * amount_loss
+    
+    def training_step(
+        self,
+        batch: tuple[
+            torch.LongTensor,
+            torch.LongTensor,
+            torch.LongTensor,
+            torch.DoubleTensor,
+            int,
+            float
+        ],
+        batch_idx: int
+    ) -> typing.Mapping[str, float]:
+        loss = self._calculate_losses(batch)
+        self.log('train_loss', loss, prog_bar=True, on_step=True)
+
+        return {'loss': loss}
+    
+    def validation_step(
+        self,
+        batch: tuple[
+            torch.LongTensor,
+            torch.LongTensor,
+            torch.LongTensor,
+            torch.DoubleTensor,
+            int,
+            float
+        ],
+        batch_idx: int
+    ) -> None:
+        loss = self._calculate_losses(batch)
+        self.log('val_loss', loss, prog_bar=True, on_step=False, on_epoch=True)
+
+    def test_step(
+        self,
+        batch: tuple[
+            torch.LongTensor,
+            torch.LongTensor,
+            torch.LongTensor,
+            torch.DoubleTensor,
+            int,
+            float
+        ],
+        batch_idx: int
+    ) -> None:
+        self.log(
+            'test_loss',
+            self._calculate_losses(batch),
+            on_epoch=True,
+            on_step=False
+        )
+
+    def configure_optimizers(self) -> typing.Mapping[str, typing.Any]:
+        opt = torch.optim.AdamW(self.parameters(), self.hparams['lr'], weight_decay=self.hparams['weight_decay'])
+        scheduler =torch.optim.lr_scheduler.ReduceLROnPlateau( opt, 'min', 1e-1, 2, verbose=True)
+        return {'optimizer': opt, 'scheduler': scheduler, 'monitor': 'val_loss'}
+
