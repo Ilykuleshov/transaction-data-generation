fcf53c3c0209f8359b59e83eefbf6e7f7e2745cc
nokiroki <nikita@nokiroki.ru>
2023-04-28 14:26:52 +0300

    lstm forward method

--- src/networks/lstm/lstm.py
+++ src/networks/lstm/lstm.py
@@ -6,6 +6,7 @@ from pytorch_lightning import LightningModule
 
 import torch
 from torch import nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 from src.networks.common_layers import PositionalEncoding
 
@@ -60,7 +61,9 @@ class LSTMAE(LightningModule):
             batch_first=True
         )
 
-        self.output_layer = nn.Linear(embed_dim * 2, n_features)
+        self.out_amount = nn.Linear(2 * embed_dim, 1)
+        self.out_binary = nn.Linear(2 * embed_dim, 1)
+        self.out_mcc    = nn.Linear(2 * embed_dim, n_vocab_size)
 
         self.train_time: float = None
 
@@ -88,4 +91,47 @@ class LSTMAE(LightningModule):
         transaction_amt: torch.Tensor,
         lengths: torch.Tensor
     ) -> None:
-        ...
+        mcc_embed = self.mcc_embed(mcc_codes)
+        mcc_embed = self.pe(mcc_embed)
+
+        is_income = torch.unsqueeze(is_income, -1)
+        transaction_amt = torch.unsqueeze(transaction_amt, -1)
+
+        mat_orig = torch.cat((mcc_embed, transaction_amt, is_income), -1)
+        # Pack sequnces to get a real hidden state no matter of difference in lengths
+        packed_mat = pack_padded_sequence(
+            mat_orig,
+            lengths.cpu(),
+            batch_first=True,
+            enforce_sorted=False
+        )
+
+        packed_mat, _ = self.encoder1(packed_mat)
+        packed_mat, _ = self.encoder2(packed_mat)
+
+        packed_mat, _ = self.decoder1(packed_mat)
+        packed_mat, _ = self.decoder2(packed_mat)
+
+        # Get original format (batch_size X 2 * embed_dim X max_length)
+        seqs_after_lstm = pad_packed_sequence(packed_mat, True)
+
+        mcc_rec = self.out_mcc(seqs_after_lstm)
+        is_income_rec = self.out_binary(seqs_after_lstm)
+        amount_rec = self.out_amount(seqs_after_lstm)
+
+        mcc_rec, is_income_rec, amount_rec = self._trim_out_seq(
+            [mcc_rec, is_income_rec, amount_rec]
+        )
+
+        return mcc_rec, is_income_rec, amount_rec
+
+    # Method for returning original padding
+    @staticmethod
+    def _trim_out_seq(
+        seqs_to_trim: typing.List[torch.Tensor],
+        lengths: torch.Tensor
+    ) -> typing.List[torch.Tensor]:
+        max_length = seqs_to_trim[0].shape[1]
+        mask = torch.arange(max_length) \
+                .expand(len(lengths), max_length) < lengths.unsqueeze(1)
+        return list(map(lambda seq: seq.masked_fill_(~mask), seqs_to_trim))
